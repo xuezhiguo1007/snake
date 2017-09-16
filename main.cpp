09977e2510d692380ee065fc047f5a2bf117f054
XueClearlove <1461680129@qq.com>
2017-09-16 19:23:50 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "mainWidget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    mainWidget w;
+    w.show();
+    return a.exec();
+}
+
